e3b26020527e6a3464793305edb0001ecf1a93a7
redangel1993 <Kamperis1993@hotmail.com>
2015-06-07 23:11:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/marios/networksecurity/AppConfig.java
@@ -0,0 +1,10 @@
+package com.example.marios.networksecurity;
+
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.4/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.4/android_login_api/";
+}
